2c7e3f29f6efecebb9a87a74b539baae7b4445ff
ksomemo <kntsmy@hotmail.co.jp>
2019-08-31 16:00:17 +0900

    Introduce tokenizer

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import sys
 import dataclasses
-from typing import Optional
+import string
+from typing import Optional, List
 
 source = ''
 source_index = 0
 
 
 def get_char() -> Optional[str]:
+    global source, source_index
     if source_index == len(source):
         # byte でなくstrをとりあえず返すので
         # 0として判断できるようなNoneを返す
         return None
 
     char = source[source_index]
-    sourceIndex =+ 1
+    source_index += 1 
     return char
 
 
 def unget_char() -> None:
+    global source_index
     source_index -= 1
 
 
@@ -26,10 +29,53 @@ class Token:
     kind: str # "intliteral"
     value: str
 
+def read_number(char: str) -> str:
+    number = [char]
+    while True:
+        char = get_char()
+        if char is None:
+            break
+
+        if '0' <= char and char <= '9':
+            number.append(char)
+        else:
+            unget_char()
+            break
+
+    return ''.join(number)
+
+
+def tokenize() -> List[Token]:
+    tokens = []
+    print('# Tokens : ', end='')
+
+    while True:
+        char = get_char()
+        if char is None:
+            break
+
+        if char in string.digits:
+            intliteral = read_number(char)
+            token = Token('intliteral', intliteral)
+            tokens.append(token)
+            print(f" '{token.value}'")
+        else:
+            message = f"tokenizer: Invalid char: '{char}'"
+            print(message, file=sys.stderr)
+            # golang panic: output exit status 2 to stderr
+            # but, $? is 1
+            sys.exit(1)
+
+    print()
+    return tokens
+
 
 def main() -> None:
+    global source
     source = sys.stdin.read() 
-    number = int(source)
+    tokens = tokenize()
+    token0 = tokens[0]
+    number = int(token0.value)
 
     print('  .global main')
     print('main:')
