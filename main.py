abd0886e972be02d117323e111b66d0fdec3c21f
ksomemo <kntsmy@hotmail.co.jp>
2019-08-31 13:48:27 +0900

    Implement get_char, unget_char

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import sys
+from typing import Optional
+
+source = ''
+source_index = 0
 
 
-def main():
+def get_char() -> Optional[str]:
+    if source_index == len(source):
+        # byte でなくstrをとりあえず返すので
+        # 0として判断できるようなNoneを返す
+        return None
+
+    char = source[source_index]
+    sourceIndex =+ 1
+    return char
+
+
+def unget_char() -> None:
+    source_index -= 1
+
+
+def main() -> None:
     source = sys.stdin.read() 
     number = int(source)
 
